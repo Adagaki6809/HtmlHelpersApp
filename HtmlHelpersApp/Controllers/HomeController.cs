16196b5e8eda0552d614949df158ed98ee5a67a4
Hitagi <include.ofstream.h@gmail.com>
2021-04-27 17:33:30 +0300

    first commit

---
+++ HtmlHelpersApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using HtmlHelpersApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlHelpersApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name, int age)
+        {
+            return Content(Url.Content("~/lib/jquery/dist/jquery.js")); //Content($"{name} - {age}");
+        }
+    }
+    
+}
